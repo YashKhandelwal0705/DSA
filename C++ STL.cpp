a66c120234ef479e17caa031c85a10da3b619cf0
Yash Khandelwal <yashkhandelwal0705@gmail.com>
2023-09-26 22:39:21 +0530

    Changed names

--- C++ STL.cpp
+++ C++ STL.cpp
@@ -26,15 +26,15 @@
 //VECTORS
 
 
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// void explainVector(){
-//     vector<int>v;
-//     v.push_back(1);
-//     v.emplace_back(2);
+void explainVector(){
+    vector<int>v;
+    v.push_back(1);
+    v.emplace_back(2);
 
-// }
+}
 
 // int main(){
 //     explainVector();
