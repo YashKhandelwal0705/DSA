a1072293f3d39938f680e9fbc9129b69ccbd6d90
Yash Khandelwal <yashkhandelwal0705@gmail.com>
2023-10-08 23:30:41 +0530

    Basic maths functions

--- basicmaths.cpp
+++ basicmaths.cpp
@@ -135,4 +135,24 @@ int sumOfAllDivisors(int n){
 
 
 
+/*Problem statement 6:Prime number check*/
+
+
+
+
+bool isPrime(int n){
+    if (n <= 1) {
+		return false;
+	}
+	for (int i = 2; i*i <= n; i++) {
+    	if (n % i == 0) {
+    	return false;
+    	}
+	}
+        return true;
+}
+
+
+
+
 
